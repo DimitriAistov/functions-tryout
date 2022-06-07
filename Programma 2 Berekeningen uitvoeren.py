5fd216fee06ec12464dde3983a777264575292f5
Dimitri <99058899@mydavinci.nl>
2022-06-07 15:29:38 +0200

    4

---
+++ Programma 2 Berekeningen uitvoeren.py
@@ -0,0 +1,23 @@
+def addition(nummer1, nummer2) ->int:
+    return print (nummer1, '+', nummer2, '=', (nummer1 + nummer2))
+addition(10,12)
+
+def subtraction(nummer1, nummer2) ->int:
+    return print (nummer1, '-', nummer2, '=', (nummer1 - nummer2))
+subtraction(58,34)
+
+def multiplication(nummer1, nummer2) ->int:
+    return print (nummer1, 'x', nummer2, '=', (nummer1 * nummer2))
+multiplication(6,7)
+
+def division(nummer1, nummer2) ->int:
+    return print (nummer1, ':', nummer2, '=', (nummer1 / nummer2))
+division(144,2)
+
+def increment(nummer1, nummer2) ->int:
+    return print (nummer1, '+', nummer2, '=', (nummer1 + nummer2))
+multiplication(12,1)
+
+def decrement(nummer1, nummer2) ->int:
+    return print (nummer1, '-', nummer2, '=', (nummer1 - nummer2))
+decrement(34,1)
